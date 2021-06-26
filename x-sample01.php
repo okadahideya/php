7fed2e293ac098bc199e03ba7a49d71298c398ea
okadahideya <hideya400@gmail.com>
2021-06-26 15:15:20 +0900

    練習

--- x-sample01.php
+++ x-sample01.php
@@ -1,3 +1,26 @@
 <?php
-  echo "Hello";
+   $myArray = array("tom","maiku","tanaka");
+   print_r($myArray);
+
+   echo "<br></br>";
+
+   echo $myArray[1];
+
+   echo "<br></br>";
+
+   $another[0] = "paiza";
+   $another[1] = "atcoder";
+   $another[5] = "hide";
+
+   print_r($another);
+
+   echo "<br></br>";
+
+   $another["key"] = "value";
+   print_r($another);
+
+   echo "<br></br>";
+
+   print_r($_GET);
+
 ?>
